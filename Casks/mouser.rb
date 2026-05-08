028841a1bcffcaef2bb271bbe951011c9b87b9d9
Brian LaMattina <brian@lamattina.io>
2026-05-08 08:07:56 -0400

    Drop unnecessary verified: parameter from mouser url stanza

--- Casks/mouser.rb
+++ Casks/mouser.rb
@@ -2,15 +2,14 @@ cask "mouser" do
   version "3.6.0"
 
   on_arm do
-    url "https://github.com/TomBadash/Mouser/releases/download/v#{version}/Mouser-macOS.zip",
-        verified: "github.com/TomBadash/Mouser/"
     sha256 "b7972daf47d506e5b256a1b9e0912b91ba5eae77b151d1ff4174ea52800a2fea"
-  end
 
+    url "https://github.com/TomBadash/Mouser/releases/download/v#{version}/Mouser-macOS.zip"
+  end
   on_intel do
-    url "https://github.com/TomBadash/Mouser/releases/download/v#{version}/Mouser-macOS-intel.zip",
-        verified: "github.com/TomBadash/Mouser/"
     sha256 "7ffcb1ab79f4af1c11e790b7fd1401a0b057bd958fb34cad1079f7342ca8e36d"
+
+    url "https://github.com/TomBadash/Mouser/releases/download/v#{version}/Mouser-macOS-intel.zip"
   end
 
   name "Mouser"
@@ -26,8 +25,8 @@ cask "mouser" do
 
   zap trash: [
     "~/Library/Application Support/Mouser",
+    "~/Library/LaunchAgents/io.github.tombadash.mouser.plist",
     "~/Library/Logs/Mouser",
     "~/Library/Preferences/io.github.tombadash.mouser.plist",
-    "~/Library/LaunchAgents/io.github.tombadash.mouser.plist",
   ]
 end
